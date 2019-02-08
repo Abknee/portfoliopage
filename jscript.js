78dc79617e7fb822c527ec69f7c5d1303f52e1fa
Shawn abney <46576633+Abknee@users.noreply.github.com>
2019-02-08 10:20:00 -0600

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('#twitter').on('click', () => {
+        $('#twitterem').toggle();
+    });
+    $('#github').on('click', () => {
+        $('.githubholder').toggle();
+    });
+    $('#linkedin').on('click', () => {
+        $('.linkedinholder').toggle();
+    });
+    $('#email').on('click', () => {
+        $('#emailem').toggle();
+    });
+
+});
